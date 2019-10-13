3e5e741cb9235877448d358025a9d07a4383b061
Anthony Huynh <ahuynh0730@gmail.com>
2019-10-13 03:34:42 -0500

    Update mypython.py
    Creating the three files

--- Block 1/Program Py/mypython.py
+++ Block 1/Program Py/mypython.py
@@ -1,2 +1,7 @@
 # Anthony Huynh's Python Program
 
+# opening files if they exist, otherwise will create
+file1= open("file1.txt", "w+")
+file2= open("file2.txt", "w+")
+file3= open("file3.txt", "w+")
+
