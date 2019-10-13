b1e06979c050d7376541f820190d512c20ff2c5a
Anthony Huynh <ahuynh0730@gmail.com>
2019-10-13 03:53:42 -0500

    Update mypython.py
    Each file is filled with 10 random lower case letters

--- Block 1/Program Py/mypython.py
+++ Block 1/Program Py/mypython.py
@@ -1,9 +1,25 @@
 # Anthony Huynh's Python Program
 
+import string
+import random
+
 # opening files if they exist, otherwise will create
 file1= open("file1.txt", "w+")
 file2= open("file2.txt", "w+")
 file3= open("file3.txt", "w+")
+
+# function that fills file passed in with 10 random lowercase letters 
+# and a newline at the end
+def fillFile(fileName):
+	for iter in range(0, 10):
+		randomChar=random.choice(string.ascii_lowercase)
+		fileName.write(randomChar)
+	fileName.write("\n")	
+
+# calling fillFile with each of the files
+fillFile(file1)
+fillFile(file2)
+fillFile(file3)
 
 #closing the files
 file1.close()
