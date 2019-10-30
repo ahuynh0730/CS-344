3eeb9423216cb74ffe66ae1f5e0fae62ba6fa80e
Anthony Huynh <ahuynh0730@gmail.com>
2019-10-30 00:53:57 -0500

    Create gitignore and buildrooms.c

---
+++ Block 2/Project2/Project2/huynhant.buildrooms.c
@@ -0,0 +1,16 @@
+// Author: Anthony Huynh
+// Class: CS 344
+// Project: Program 2 - adventure
+// Date Due:
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]) {
+
+	//gets current process id
+
+
+	return 0;
+}
