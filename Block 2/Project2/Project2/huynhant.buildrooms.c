67e4288ec6a1edd2cc3379f28561988f72f2341a
Anthony Huynh <ahuynh0730@gmail.com>
2019-10-30 02:10:30 -0500

    Directory made with process id

--- Block 2/Project2/Project2/huynhant.buildrooms.c
+++ Block 2/Project2/Project2/huynhant.buildrooms.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 
@@ -17,8 +18,17 @@ int main(int argc, char* argv[]) {
 	//gets current process id
 	pid_t processId;
 	processId = getpid();
+	printf("The process id is: %d\n", processId);
 
-	printf("The process id is %d\n", processId);
+	//creates array of characters and initializes to null
+	char directoryName[25];
+	memset(directoryName, '\0', 25);
+
+	//fills array with name of directory and process id
+	snprintf(directoryName, 25, "huynhant.rooms.%d", processId);
+
+	//makes directory
+	mkdir(directoryName);
 
 
 	return 0;
