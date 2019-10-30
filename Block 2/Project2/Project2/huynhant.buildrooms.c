93020cb07099b87306db21d929e3002b82219cf0
Anthony Huynh <ahuynh0730@gmail.com>
2019-10-30 01:34:04 -0500

    Program gets process id

--- Block 2/Project2/Project2/huynhant.buildrooms.c
+++ Block 2/Project2/Project2/huynhant.buildrooms.c
@@ -1,15 +1,24 @@
 // Author: Anthony Huynh
 // Class: CS 344
 // Project: Program 2 - adventure
-// Date Due:
+// Date Due: 11/1/2019
+
+//this program does not run on windows, must be run in a linux setting
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 
 int main(int argc, char* argv[]) {
 
 	//gets current process id
+	pid_t processId;
+	processId = getpid();
+
+	printf("The process id is %d\n", processId);
 
 
 	return 0;
