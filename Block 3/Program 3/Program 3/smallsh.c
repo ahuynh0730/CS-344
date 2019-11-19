70844baf8455ca467ba58900c22b7d317b8f9331
Anthony Huynh <ahuynh0730@gmail.com>
2019-11-18 19:08:15 -0600

    Update smallsh.c
    Creates child process

--- Block 3/Program 3/Program 3/smallsh.c
+++ Block 3/Program 3/Program 3/smallsh.c
@@ -6,8 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #define PROMPT_REQUEST ":"
+#define MAX_ARGUMENTS 512
 
 int main(int argc, char* argv[]) {
 
@@ -16,10 +20,15 @@ int main(int argc, char* argv[]) {
 	
 	int i;
 	char* parsedInput;
-	char* arguments[512];
+	char* arguments[MAX_ARGUMENTS];
 	int currentArgument = 0;
+	char cwd[256];
+	pid_t cpid;
+	int status = 0;
 
-
+	for (i = 0; i < MAX_ARGUMENTS; i++) {
+		arguments[i] = NULL;
+	}
 
 	while (1) {
 		//prompt and flushes 
@@ -30,14 +39,12 @@ int main(int argc, char* argv[]) {
 		memset(userInput, '\0', sizeof(userInput));
 		fgets(userInput, sizeof(userInput), stdin);
 
-		//parsing user input 
-		parsedInput = strtok(userInput, " ");
-		
-		//if first argument was exit
-		if (strcmp(parsedInput, "exit") == 0) {
-			exit(0);
-		}
+		//will remove trailing \n at end of user input and replace with \0
+		userInput[strlen(userInput) - 1] = '\0';
 
+		//parsing user input, if no space, null pointer will be returned
+		parsedInput = strtok(userInput, " ");
+			
 		//will loop through each space separated text and place in arguments
 		while (parsedInput != NULL) {
 			arguments[currentArgument] = malloc(100 * sizeof(char));
@@ -46,18 +53,74 @@ int main(int argc, char* argv[]) {
 			parsedInput = strtok(NULL, " ");
 		}
 
+		//if there were no arguments or the first argument started with #, will do nothing
+		if (arguments[0] == NULL || arguments[0][0] == '#') {
+			printf("This should display if no arguments or is a comment\n");
+		}
+
+		//will exit upon request, also freeing any memory that was malloc'd
+		else if (strcmp(arguments[0], "exit") == 0) {
+			for (i = 0; i < currentArgument; i++) {
+				free(arguments[i]);
+			}
+			exit(0);
+		}
+			
+		
+		//to change directories, if no directory is stated, will go to home
+		else if (strcmp(arguments[0], "cd") == 0) {
+			if (arguments[1] == NULL) {                        
+				chdir(getenv("HOME"));
+			}
+			else {
+				chdir(arguments[1]);
+			}
+		}
+
+		//prints either exit status or the terminating signal of the last foreground process
+		else if (strcmp(arguments[0], "status") == 0) {
+			;
+		}
+
+		//all other commands that are not built in
+		else {
+			cpid = fork();
+			switch (cpid) {
+
+			//if something went wrong during fork
+			case -1:
+				perror("Hull breach!");
+				status = 1;
+				break;
+
+			//if the process is the child process
+			case 0:
+				break;
+
+			//if the process is the parent
+			default:
+				break;
+			}
+		}
+			
+		/*
 		for (i = 0; i < currentArgument; i++) {
 			printf("Argument %d: %s", i + 1, arguments[i]);
 			if (i != currentArgument - 1) {
 				printf("\n");
 			}
 		}
-		
+		*/
+			
 
+		//frees all memory malloc'd and sets currentArguments to 0 before prompting for more input
 		for (i = 0; i < currentArgument; i++) {
 			free(arguments[i]);
-			currentArgument = 0;
+			arguments[i] = NULL;
 		}
+		currentArgument = 0;
+		
+		
 	
 	}
 	return 0;
