0cee59747c9654d8ff19fa8faee925f78532a615
Anthony Huynh <ahuynh0730@gmail.com>
2019-11-20 03:16:02 -0600

    Update smallsh.c
    Background/foreground process finished

--- Block 3/Program 3/Program 3/smallsh.c
+++ Block 3/Program 3/Program 3/smallsh.c
@@ -15,6 +15,19 @@
 #define PROMPT_REQUEST ":"
 #define MAX_ARGUMENTS 512
 
+void showStatus(int childExitMethod) {
+	//code from slides
+	if (WIFEXITED(childExitMethod)) {
+		int exitStatus = WEXITSTATUS(childExitMethod);
+		printf("exit value %d\n", exitStatus);
+		fflush(stdout);
+	}
+	else {
+		printf("Child terminated by a signal\n");
+		fflush(stdout);
+	}
+}
+
 int main(int argc, char* argv[]) {
 
 	//2049 is to allow for 2048 characters plus terminator at end
@@ -31,12 +44,15 @@ int main(int argc, char* argv[]) {
 	int outputFile = 0;
 	int inputFile = 0;
 	int result = 0;
+	int foregroundProcess = 1;
 
 	for (i = 0; i < MAX_ARGUMENTS; i++) {
 		arguments[i] = NULL;
 	}
 
 	while (1) {
+		foregroundProcess = 1;
+
 		//prompt and flushes 
 		printf("%s ", PROMPT_REQUEST);
 		fflush(stdout);
@@ -77,11 +93,24 @@ int main(int argc, char* argv[]) {
 				parsedInput = strtok(NULL, " ");
 			}
 		}
+		
+		//if last argument was a &, will turn process into background process
+		//will also free last argument and set to null
+		//will only check for & if there is at least one argument
+		if (currentArgument != 0) {
+			if (strcmp(arguments[currentArgument - 1], "&") == 0) {
+				foregroundProcess = 0;
+				free(arguments[currentArgument - 1]);
+				arguments[currentArgument - 1] = NULL;
+				currentArgument--;
+			}
+		}
 
 		//if there were no arguments or the first argument started with #, will do nothing
 		if (arguments[0] == NULL || arguments[0][0] == '#') {
-			printf("This should display if no arguments or is a comment\n");
+			;
 		}
+		
 
 		//will exit upon request, also freeing any memory that was malloc'd
 		else if (strcmp(arguments[0], "exit") == 0) {
@@ -91,7 +120,6 @@ int main(int argc, char* argv[]) {
 			exit(0);
 		}
 			
-		
 		//to change directories, if no directory is stated, will go to home
 		else if (strcmp(arguments[0], "cd") == 0) {
 			if (arguments[1] == NULL) {                        
@@ -104,15 +132,7 @@ int main(int argc, char* argv[]) {
 
 		//prints either exit status or the terminating signal of the last foreground process
 		else if (strcmp(arguments[0], "status") == 0) {
-
-			//code from slides
-			if (WIFEXITED(childExitMethod)){
-				int exitStatus = WEXITSTATUS(childExitMethod);
-				printf("exit value %d\n", exitStatus);
-			}
-			else {
-				printf("Child terminated by a signal\n");
-			}
+			showStatus(childExitMethod);
 		}
 
 		//all other commands that are not built in
@@ -139,7 +159,7 @@ int main(int argc, char* argv[]) {
 						_Exit(1);
 					}
 
-					//will redirect stdout to outputFile
+					//will redirect stdin to inputFile
 					result = dup2(inputFile, 0);
 					if (result == -1) {
 						perror("dup2 error");
@@ -165,6 +185,39 @@ int main(int argc, char* argv[]) {
 					close(outputFile);
 				}
 
+				//will redirect input/output files to /dev/null if not specified and if background process
+				if (!foregroundProcess) {
+					if (inputFileName == NULL) {
+						inputFile = open("/dev/null", O_RDONLY);
+						if (inputFile == -1) {
+							perror("cannot open /dev/null for reading");
+							_Exit(1);
+						}
+
+						result = dup2(inputFile, 0);
+						if (result == -1) {
+							perror("dup2 error");
+							_Exit(1);
+						}
+						close(inputFile);
+					}
+
+					if (outputFileName == NULL) {
+						outputFile = open("/dev/null", O_WRONLY | O_TRUNC | O_CREAT, 0700);
+						if (outputFile == -1) {
+							perror("cannot open /dev/null for writing");
+							_Exit(1);
+						}
+
+						result = dup2(outputFile, 1);
+						if (result == -1) {
+							perror("dup2 error");
+							_Exit(1);
+						}
+						close(outputFile);
+					}
+				}
+
 				//if execvp fails, will return a negative one and alert the user
 				if (execvp(arguments[0], arguments) < 0) {
 					printf("%s: no such file or directory\n", arguments[0]);
@@ -175,8 +228,16 @@ int main(int argc, char* argv[]) {
 
 			//if the process is the parent
 			default:
-				waitpid(cpid, &childExitMethod, 0);
-				break;
+				if (foregroundProcess) {
+					waitpid(cpid, &childExitMethod, 0);
+					break;
+				}
+
+				else {
+					printf("background pid is %d\n", cpid);
+					fflush(stdout);
+
+				}
 			}
 		}
 			
@@ -193,7 +254,13 @@ int main(int argc, char* argv[]) {
 		inputFileName = NULL;
 		outputFileName = NULL;
 		
-		
+		//will check if any children have finished before new prompt
+		cpid = waitpid(-1, &childExitMethod, WNOHANG);
+		while (cpid > 0) {
+			printf("background pid %d is done: ", cpid);
+			showStatus(childExitMethod);
+			cpid = waitpid(-1, &childExitMethod, WNOHANG);
+		}
 	
 	}
 	return 0;
