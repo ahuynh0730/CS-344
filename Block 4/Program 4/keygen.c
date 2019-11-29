26c8d28c4f1039a5fe4a6f3292261888e4b76b29
Anthony Huynh <ahuynh0730@gmail.com>
2019-11-29 01:52:35 -0600

    Update keygen.c
    Random key generated and displayed to stdout

--- Block 4/Program 4/keygen.c
+++ Block 4/Program 4/keygen.c
@@ -3,9 +3,48 @@
 // Project: Program 44 - OTP
 // Date Due: 12/6/2019
 
-int main(int argc, char* argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]) {
 	
+	srand((unsigned) time(0));
+	int i = 0;
+	char* endPtr = NULL;
+	char* key = NULL;
+	int randomNumber = 0;
+	
+	//will inform user if there are not 2 arguments
+	if (argc != 2){
+		fprintf(stderr, "Wrong number of arguments. Please try again!\n");
+		exit(0);
+	}
+	
+	//converts first argument into a long int
+	long lengthOfKey = strtol(argv[1], &endPtr, 0);
+	
+	//increments by 1 to account for newline char
+	lengthOfKey++;
+	key = malloc(lengthOfKey * sizeof(char));
+	
+	//loop that will create a new random number and convert to ASCII char each iteration
+	for (i = 0; i < lengthOfKey - 1 ; i++){
+		randomNumber = rand() % 27;
+		switch (randomNumber){
+			//if random number was 0, will convert to space
+			case 0:
+				key[i] = (char)randomNumber +  32;
+				break;
+				
+			//all other numbers will convert to matching letter
+			default:
+				key[i] = (char)randomNumber + 64;
+				break;
+		}
+	}
+	key[lengthOfKey - 1] = '\n';
+	
+	fprintf(stdout, "%s", key);
 	
 	return 0;	
 }
